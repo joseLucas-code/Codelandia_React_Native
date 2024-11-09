bf4b3e41f30124ea0b8d3f8cae92bb85aaadbce6
Drummonds <skyleer05@gmail.com>
2024-11-08 21:01:52 -0300

    new: gitignore and initial desafio_4

---
+++ desafio_4/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import '../style/global.css';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({});
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ desafio_4/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl text-red-400">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </SafeAreaView>
+  );
+}
